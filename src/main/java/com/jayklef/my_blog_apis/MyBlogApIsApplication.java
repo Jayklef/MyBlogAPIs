82a95a428a2db35880d064052023bca3d70fbee1
Jayklef <jerryarhawho@gmail.com>
2023-10-27 16:33:55 +0100

    Initial project setup

---
+++ src/main/java/com/jayklef/my_blog_apis/MyBlogApIsApplication.java
@@ -0,0 +1,13 @@
+package com.jayklef.my_blog_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogApIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBlogApIsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jayklef/my_blog_apis/MyBlogApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayklef.my_blog_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApIsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
