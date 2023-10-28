63353ab6c54052b1f996c34491bebfe4fc183f6e
Jayklef <jerryarhawho@gmail.com>
2023-10-28 01:41:37 +0100

    PostDto, PostService, impl, PostController

---
+++ src/main/java/com/jayklef/my_blog_apis/dto/PostDto.java
@@ -0,0 +1,11 @@
+package com.jayklef.my_blog_apis.dto;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+    private Long id;
+    private String title;
+    private String description;
+    private String content;
+}
---
+++ src/main/java/com/jayklef/my_blog_apis/service/PostService.java
@@ -0,0 +1,8 @@
+package com.jayklef.my_blog_apis.service;
+
+import com.jayklef.my_blog_apis.dto.PostDto;
+
+public interface PostService {
+
+    PostDto createPost(PostDto postDto);
+}
